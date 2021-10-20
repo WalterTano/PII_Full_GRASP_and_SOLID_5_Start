314b0469e46de164501f9f0d92ff003fd52d6ca5
Walter Taño <tano.walter@hotmail.com>
2021-10-20 19:16:16 -0300

    Ejercicio completado, interfaz IPrintable añadida

---
+++ src/Library/IPrintable.cs
@@ -0,0 +1,16 @@
+//-------------------------------------------------------------------------
+// <copyright file="Recipe.cs" company="Universidad Católica del Uruguay">
+// Copyright (c) Programación II. Derechos reservados.
+// </copyright>
+//-------------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+
+namespace Full_GRASP_And_SOLID
+{
+    public interface IPrintable
+    {
+        string GetTextToPrint();
+    }
+}
